f139b6692b192e7c211c698a6b98383424e8abd5
tenthirtyone <alexsherbuck@gmail.com>
2024-08-15 23:05:38 -0500

    add zerodev

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Main from "./Main";
 const App = () => (
   <DynamicContextProvider
     settings={{
-      environmentId: "2762a57b-faa4-41ce-9f16-abff9300e2c9",
+      environmentId: process.env.REACT_APP_DYNAMIC_PROJECT_KEY,
       walletConnectors: [EthereumWalletConnectors],
     }}
   >
